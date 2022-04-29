276595fe722c72dbe50522773f955cc37ed57cf4
Dary <dary.cc@gmail.com>
2022-04-29 07:01:40 -0500

    jala 22

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+# run "xrandr --output eDP-1-1 --primary --mode 1920x1080 --rotate normal --output HDMI-0 --mode 2560x1080 --rotate normal --right-of eDP-1-1"
+xrandr --output eDP-1-1 --primary --mode 1920x1080 --rotate normal --output HDMI-0 --mode 2560x1080 --rotate normal --right-of eDP-1-1
+# echo "Hello World"
