2a694db31d9907f18ff5b69809c9a7a58915e170
DaryCC <dary.cc@gmail.com>
2023-02-20 22:31:39 -0600

    Nueva configuración de Xrander
    *** Falta por arreglar

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -10,6 +10,7 @@ xrandr --output eDP-1-1 --primary --mode 1920x1080 \
        --mode 2560x1080 --rotate normal --right-of eDP-1-1
 bspc monitor eDP-1-1 -d 1 2 3 4
 bspc monitor HDMI-0  -d 5 6 7 8
+echo "Configuracion para monitor externo aplicada..."
 # elif
 #     bspc monitor eDP-1-1 -d 1 2 3 4 5 6 8
 fi
@@ -31,6 +32,5 @@ fi
 # # Launch bar1 and bar2
 # # polybar example &
 
-echo "Configuracion para monitor externo aplicada..."
 #####APLICACIONES DE INICIO#####################
 
